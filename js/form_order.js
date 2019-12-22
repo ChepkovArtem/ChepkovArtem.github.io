00a822e72a6fc9475a4460009828a1a3b3b924a0
Chepkov Artem <vega4frost@yandex.ru>
2019-12-23 02:27:08 +0300

    new changes

--- js/form_order.js
+++ js/form_order.js
@@ -4,5 +4,49 @@ const sendButton = document.querySelector('#make-order');
 sendButton.addEventListener('click', function(event){
     event.preventDefault();
     console.log(myForm.elements.name.value);
-    console.log(myForm.elements.name.value);
-})
+    console.log(myForm.elements.phone.value);
+    console.log(myForm.elements.comment.value);
+
+    if (validateForm(myForm)){
+        const data ={
+            name: myForm.elements.name.value,
+            phone: myForm.elements.phone.value,
+            comment: myForm.elements.comment.value
+        };
+
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
+        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+        xhr.send(JSON.stringify(data));
+        xhr.addEventListener('load', ()=>{
+            console.log(xhr.response);
+        });
+    }
+});
+
+function validateForm(form){
+    let valid = true;
+
+    if(!validateField(form.elements.name)){
+        valid = false;
+    }
+    if(!validateField(form.elements.phone)){
+        valid = false;
+    }
+    if(!validateField(form.elements.comment)){
+        valid = false;
+    }
+    return valid;
+}
+
+function validateField(field){
+    if(!field.checkValidity()){
+        field.nextElementSibling.textContent = field.validationMessage;
+        return false;
+    }
+    else{
+        field.nextElementSibling.textContent = '';
+        console.log('ок!');
+        return field.checkValidity();
+    }
+}
---
+++ js/one-page.js
@@ -0,0 +1,64 @@
+const sections = $(".section");
+const display = $(".maincontent");
+let inScroll = false;
+
+const performTransition = sectionEq =>{
+    if (inScroll === false){
+        inScroll = true;
+        const position = sectionEq * - 100;
+
+        sections
+            .eq(sectionEq)
+            .addClass("active")
+            .siblings()
+            .removeClass("active");
+
+    display.css({
+        transform: `translateY(${position}%)`
+    });
+
+    setTimeout(()=>{
+        inScroll=false;
+    }, 800);
+}
+};
+
+const scrollToSection = direction =>{
+    const activeSecton = sections.filter('.active');
+    const nextSection = activeSecton.next();
+    const prevSection = activeSecton.prev();
+
+    if(direction === "next" && nextSection.length){
+        performTransition(nextSection.index());
+    }
+
+    if(direction === "prev" && prevSection.length){
+        performTransition(prevSection.index());
+    }
+};
+
+$(window).on("wheel",e=>{
+    const deltaY = e.originalEvent.deltaY;
+
+    if(deltaY > 0){
+        scrollToSection("next");
+    }
+
+    if(deltaY < 0){
+        scrollToSection("prev");
+    }
+});
+
+$(document).on("keydown", e=>{
+    const tagName = e.target.tagName.toLowerCase();
+    if (tagName !== "input" && tagName !== "textarea") {
+        switch (e.keyCode){
+            case 38:
+            scrollToSection("prev");
+            break;
+            case 40:
+            scrollToSection("next");
+            break;
+        }
+    }
+});
